890261bf3e54e56687e1d0b278782b87f3675aeb
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-06-21 13:17:43 +0300

    product on click  tex appear

--- js/script.js
+++ js/script.js
@@ -8,3 +8,23 @@ $(document).ready(function(){
         $('#design').slideToggle();
         $('#design-text').slideToggle();
     });
+     //Developement on click text appear
+     $('#developement').click(function(){
+        $('#developement').slideToggle();
+        $('#developement-text').slideToggle();
+    });
+    $('#developement-text').click(function(){
+        $('#developement').slideToggle();
+        $('#developement-text').slideToggle();
+    })
+    //Product on click text appear
+    $('#product').click(function(){
+        $('#product').slideToggle();
+        $('#product-text').slideToggle();
+    });
+    $('#product-text').click(function(){
+        $('#product').slideToggle();
+        $('#product-text').slideToggle();
+    });
+
+
