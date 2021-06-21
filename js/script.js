0d5fc677e1347b95d8a77ac80e621cc29466c53e
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-06-21 13:09:28 +0300

    designed on click text appear

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    //Design on click text Appear
+    $('#design').click(function(){
+        $('#design').slideToggle();
+        $('#design-text').slideToggle();
+    });
+    $('#design-text').click(function(){
+        $('#design').slideToggle();
+        $('#design-text').slideToggle();
+    });
